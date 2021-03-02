c9c45ea63a72749220f86390a4f2b4f87ca1c3fc
zeritte <zeritte@gmail.com>
2021-03-02 15:33:13 +0300

    time to push some stuff

--- lambda/index.js
+++ lambda/index.js
@@ -5,27 +5,26 @@ const Alexa = require("ask-sdk-core");
 
 const LaunchRequestHandler = {
   canHandle(handlerInput) {
-    return Alexa.getRequestType(handlerInput.requestEnvelope) === "LaunchRequest";
+    return handlerInput.requestEnvelope.request.type === "LaunchRequest";
   },
-  handle(handlerInput) {
-    const speakOutput = "Welcome, you can say Hello or Help. Which would you like to try?";
-    return handlerInput.responseBuilder.speak(speakOutput).reprompt(speakOutput).getResponse();
+  async handle(handlerInput) {
+    let message;
+    let reprompt;
+
+    message = "Welcome to the AWS Podcast. you can ask to play the audio to begin the podcast.";
+    reprompt = "You can say, play the audio, to begin.";
+    return handlerInput.responseBuilder.speak(message).reprompt(reprompt).getResponse();
   }
 };
-const HelloWorldIntentHandler = {
-  canHandle(handlerInput) {
-    return (
-      Alexa.getRequestType(handlerInput.requestEnvelope) === "IntentRequest" &&
-      Alexa.getIntentName(handlerInput.requestEnvelope) === "HelloWorldIntent"
-    );
+
+const StartPlaybackHandler = {
+  async canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === "IntentRequest" && request.intent.name === "PlayAudio";
   },
   handle(handlerInput) {
-    console.log("123123123", handlerInput.requestEnvelope);
-    const speakOutput = "Hello World!!! Testing";
-    return handlerInput.responseBuilder
-      .speak(speakOutput)
-      .reprompt("add a reprompt if you want to keep the session open for the user to respond")
-      .getResponse();
+    console.log("cameee")
+    return controller.play(handlerInput);
   }
 };
 const HelpIntentHandler = {
@@ -56,14 +55,29 @@ const CancelAndStopIntentHandler = {
 };
 const SessionEndedRequestHandler = {
   canHandle(handlerInput) {
-    return Alexa.getRequestType(handlerInput.requestEnvelope) === "SessionEndedRequest";
+    return handlerInput.requestEnvelope.request.type === "SessionEndedRequest";
   },
   handle(handlerInput) {
-    // Any cleanup logic goes here.
+    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+
     return handlerInput.responseBuilder.getResponse();
   }
 };
-
+const CheckAudioInterfaceHandler = {
+  async canHandle(handlerInput) {
+    const audioPlayerInterface = (
+      (((handlerInput.requestEnvelope.context || {}).System || {}).device || {})
+        .supportedInterfaces || {}
+    ).AudioPlayer;
+    return audioPlayerInterface === undefined;
+  },
+  handle(handlerInput) {
+    return handlerInput.responseBuilder
+      .speak("Sorry, this skill is not supported on this device")
+      .withShouldEndSession(true)
+      .getResponse();
+  }
+};
 // The intent reflector is used for interaction model testing and debugging.
 // It will simply repeat the intent the user said. You can create custom handlers
 // for your intents by defining them above, then also adding them to the request
@@ -100,13 +114,93 @@ const ErrorHandler = {
   }
 };
 
+/* HELPER FUNCTIONS */
+
+const controller = {
+  async play(handlerInput) {
+    const { attributesManager, responseBuilder } = handlerInput;
+
+    const playbackInfo = await getPlaybackInfo(handlerInput);
+    const { playOrder, offsetInMilliseconds, index } = playbackInfo;
+
+    const playBehavior = "REPLACE_ALL";
+    const podcast =
+      "https://assetstore.aflr.io/aflorithmic/libriopilot/eichhornpilot/animalsstorygerman__username~sarah.mp3";
+    const token = playOrder[index];
+    playbackInfo.nextStreamEnqueued = false;
+
+    responseBuilder
+      .speak(`This is ${podcast.title}`)
+      .withShouldEndSession(true)
+      .addAudioPlayerPlayDirective(playBehavior, podcast.url, token, offsetInMilliseconds, null);
+
+    if (await canThrowCard(handlerInput)) {
+      const cardTitle = `Playing ${podcast.title}`;
+      const cardContent = `Playing ${podcast.title}`;
+      responseBuilder.withSimpleCard(cardTitle, cardContent);
+    }
+
+    return responseBuilder.getResponse();
+  },
+  stop(handlerInput) {
+    return handlerInput.responseBuilder.addAudioPlayerStopDirective().getResponse();
+  },
+  async playNext(handlerInput) {
+    const {
+      playbackInfo,
+      playbackSetting
+    } = await handlerInput.attributesManager.getPersistentAttributes();
+
+    const nextIndex = (playbackInfo.index + 1) % constants.audioData.length;
+
+    if (nextIndex === 0 && !playbackSetting.loop) {
+      return handlerInput.responseBuilder
+        .speak("You have reached the end of the playlist")
+        .addAudioPlayerStopDirective()
+        .getResponse();
+    }
+
+    playbackInfo.index = nextIndex;
+    playbackInfo.offsetInMilliseconds = 0;
+    playbackInfo.playbackIndexChanged = true;
+
+    return this.play(handlerInput);
+  },
+  async playPrevious(handlerInput) {
+    const {
+      playbackInfo,
+      playbackSetting
+    } = await handlerInput.attributesManager.getPersistentAttributes();
+
+    let previousIndex = playbackInfo.index - 1;
+
+    if (previousIndex === -1) {
+      if (playbackSetting.loop) {
+        previousIndex += constants.audioData.length;
+      } else {
+        return handlerInput.responseBuilder
+          .speak("You have reached the start of the playlist")
+          .addAudioPlayerStopDirective()
+          .getResponse();
+      }
+    }
+
+    playbackInfo.index = previousIndex;
+    playbackInfo.offsetInMilliseconds = 0;
+    playbackInfo.playbackIndexChanged = true;
+
+    return this.play(handlerInput);
+  }
+};
+
 // The SkillBuilder acts as the entry point for your skill, routing all request and response
 // payloads to the handlers above. Make sure any new handlers or interceptors you've
 // defined are included below. The order matters - they're processed top to bottom.
 exports.handler = Alexa.SkillBuilders.custom()
   .addRequestHandlers(
+    //CheckAudioInterfaceHandler,
     LaunchRequestHandler,
-    HelloWorldIntentHandler,
+    StartPlaybackHandler,
     HelpIntentHandler,
     CancelAndStopIntentHandler,
     SessionEndedRequestHandler,
