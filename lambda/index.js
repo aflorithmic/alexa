cb413f1ad29a07bb1ff3f7ddd295fc7adda779a3
zeritte <zeritte@gmail.com>
2021-03-02 18:31:02 +0300

    remove duplicate functions

--- lambda/index.js
+++ lambda/index.js
@@ -102,17 +102,6 @@ const PlaySoundIntentHandler = {
   }
 };
 
-const SessionEndedRequestHandler = {
-  canHandle(handlerInput) {
-    return Alexa.getRequestType(handlerInput.requestEnvelope) === "SessionEndedRequest";
-  },
-  handle(handlerInput) {
-    console.log("SessionEndedRequestHandler");
-    // Any cleanup logic goes here.
-    return handlerInput.responseBuilder.getResponse();
-  }
-};
-
 // Generic error handling to capture any syntax or routing errors. If you receive an error
 // stating the request handler chain is not found, you have not implemented a handler for
 // the intent being invoked or included it in the skill builder below.
